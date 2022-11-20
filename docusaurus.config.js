4da4a9bea282034a130f41a1d7a8064c4050f197
xh321 <xiaohe321@outlook.com>
2022-11-20 19:05:31 +0800

    reclone with beta version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '火蚁校园文档站',
+  title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://doc.fireant-campus.cn',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
